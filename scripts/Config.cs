29756fffb59aed62fc85a717b99615169ebdd4a2
Miksu2001 <97743737+Miksu2001@users.noreply.github.com>
2025-02-07 23:41:42 +0200

    Feat: Map inputs and add them to Config.cs
    Map inputs for following actions:
    - TurnUp
    - TurnDown
    - TurnLeft
    - TurnRight
    
    Add the names of the inputs to Config.cs so it is easier to refer to them later.

--- scripts/Config.cs
+++ scripts/Config.cs
@@ -5,5 +5,9 @@ namespace SnakeGame
 {
     public partial class Config
     {
+        public const string ACTION_TURN_UP = "TurnUp";
+        public const string ACTION_TURN_DOWN = "TurnDown";
+        public const string ACTION_TURN_LEFT = "TurnLeft";
+        public const string ACTION_TURN_RIGHT = "TurnRight";
     }
 }
