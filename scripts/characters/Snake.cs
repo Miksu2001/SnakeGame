73dd09e2586ef506e9cd31b4e47f5625e7faa8dc
Miksu2001 <97743737+Miksu2001@users.noreply.github.com>
2025-02-07 23:58:57 +0200

    Feat: Implement input handling for snake movement direction
    - Add Direction enum that includes all the cardinal directions (up, down, left, right) and an "empty" value (used when input is not detected).
    - Add ReadInput method that reads the user input and returns the direction using the Direction enum. Additionally, use the out parameter isInputDetected to know if input is given or not.

--- scripts/characters/Snake.cs
+++ scripts/characters/Snake.cs
@@ -5,5 +5,64 @@ namespace SnakeGame
 {
     public partial class Snake : Node2D
     {
+        public enum Direction
+        {
+            NONE,
+            UP,
+            DOWN,
+            LEFT,
+            RIGHT
+        }
+
+
+
+
+
+        public override void _Ready()
+        {
+        }
+
+        public override void _Process(double delta)
+        {
+        }
+
+
+
+
+        /// <summary>
+        /// Read the input from the player and return the direction.
+        /// </summary>
+        /// <param name="isInputDetected">true = input is detected, false = input is not detected</param>
+        /// <returns></returns>
+        private Direction ReadInput(out bool isInputDetected)
+        {
+            Direction direction;
+
+            isInputDetected = true;
+
+            if (Input.IsActionJustPressed(Config.ACTION_TURN_UP))
+            {
+                direction = Direction.UP;
+            }
+            else if (Input.IsActionJustPressed(Config.ACTION_TURN_DOWN))
+            {
+                direction = Direction.DOWN;
+            }
+            else if (Input.IsActionJustPressed(Config.ACTION_TURN_LEFT))
+            {
+                direction = Direction.LEFT;
+            }
+            else if (Input.IsActionJustPressed(Config.ACTION_TURN_RIGHT))
+            {
+                direction = Direction.RIGHT;
+            }
+            else
+            {
+                direction = Direction.NONE;
+                isInputDetected = false;
+            }
+
+            return direction;
+        }
     }
 }
